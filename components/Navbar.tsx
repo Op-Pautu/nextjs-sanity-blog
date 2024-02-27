3b70630a803e0c3a3273bc2ac157c9b4b888796c
Op-Pautu <pautuop@gmail.com>
2024-02-27 15:05:36 +0530

    Add dark mode toggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import { ModeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
     <nav className="flex max-w-2xl mx-auto px-4 py-5 relative items-center w-full justify-between">
       <Link href="/" className="font-bold text-3xl">
-        Op<span className="text-sky-600">Blog</span>
+        Op<span className="text-primary">Blog</span>
       </Link>
+      <ModeToggle />
     </nav>
   );
 };
